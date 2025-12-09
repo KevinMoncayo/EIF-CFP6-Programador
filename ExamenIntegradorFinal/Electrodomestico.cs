510b2de774fb4193c3d9fdc41fa897206dae0e23
KevinMoncayo <nano2916@gmail.com>
2025-12-09 19:47:07 -0300

    Creación clase base electrodomestico

--- ExamenIntegradorFinal/Electrodomestico.cs
+++ ExamenIntegradorFinal/Electrodomestico.cs
@@ -9,15 +9,107 @@ namespace ExamenIntegradorFinal
     public abstract class Electrodomestico
     {
         #region Atributos
+
+        protected int id;
+        protected string marca;
+        protected string modelo;
+        protected double consumoBaseWatts;
+
         #endregion
 
         #region Constructores
+
+        public Electrodomestico(string marca, string modelo, double consumo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            ConsumoBaseWatts = consumo;
+        }
+
         #endregion
 
         #region Propiedades
+
+        protected abstract string CategoriaEnergetica 
+        {
+            get;
+        }
+
+        public int Id 
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Marca 
+        {
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El campo no puede estar vacío!");
+                }
+                this.marca = value;
+            }
+        }
+
+        public string Modelo
+        {
+            get
+            {
+                return modelo;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El campo no puede estar vacío!");
+                }
+                this.modelo = value;
+            }
+        }
+
+        public double ConsumoBaseWatts 
+        {
+            get
+            {
+                return consumoBaseWatts;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("El valor no puede ser negativo!");
+                }
+                this.consumoBaseWatts = value;
+            }
+        }
+
         #endregion
 
         #region Metodos
+
+        public abstract double CalcularCostoMensual(int horasDiarias, double costoPorWatt);
+        
+        public string ObtenerInformacionBase()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Marca: {Marca}");
+            sb.AppendLine($"Modelo: {Modelo}");
+            sb.Append($"Consumo base: {ConsumoBaseWatts}\n");
+            return sb.ToString();
+        }
+
         #endregion
     }
 }
